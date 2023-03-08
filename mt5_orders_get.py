9bfcdf390fb63e86239aa8a175fa0470eee74903
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-03-08 18:56:56 -0300

    orders_get() adicionado

---
+++ mt5_orders_get.py
@@ -0,0 +1,35 @@
+import MetaTrader5 as mt5
+import pandas as pd
+
+from main_functions import *
+
+# Inicialização ======================================
+intialize_mt5()
+symbol = set_symbol("ITUB4")
+
+# exibimos informações sobre ordens ativas do símbolo ITUB4
+orders=mt5.orders_get(symbol="ITUB4")
+if orders is None:
+    print(f"Sem orders em ITUB4, error code={mt5.last_error()}")
+else:
+    print(f"Total orders em ITUB4: {len(orders)}")
+    # exibimos todas as ordens ativas
+    for order in orders:
+        print(order)
+ 
+# definimos o grupo de pesquisa
+group_search = "*USD*"
+
+# obtemos uma lista de ordens com base em símbolos cujos nomes contenham "*BRL*"
+gbp_orders=mt5.orders_get(group=group_search)
+if gbp_orders is None:
+    print(f"No orders with group={group_search}, error code={mt5.last_error()}")
+else:
+    print(f"orders_get(group={group_search})={len(gbp_orders)}")
+    # exibimos essas posições como uma tabela usando pandas.DataFrame
+    df=pd.DataFrame(list(gbp_orders),columns=gbp_orders[0]._asdict().keys())
+    df.drop(['time_done', 'time_done_msc', 'position_id', 'position_by_id', 'reason', 'volume_initial', 'price_stoplimit'], axis=1, inplace=True)
+    df['time_setup'] = pd.to_datetime(df['time_setup'], unit='s')
+ 
+# concluímos a conexão ao terminal MetaTrader 5
+mt5.shutdown()
--- mt5_positions.py
+++ mt5_positions.py
@@ -21,6 +21,15 @@ else:
 group_search = "*USD*"
 
 # obtemos uma lista de posições com base em símbolos cujos nomes contenham "*USD*"
+'''
+Lembrando que a pesquisa por grupo permite várias condições separadas por vírgulas. 
+A condição pode ser especificada como uma máscara usando '*'. 
+Para exclusões, pode-se usar o símbolo de negação lógica '!'. 
+Neste caso, todas as condições são aplicadas sequencialmente, ou seja, primeiro deve-se especificar as condições para inclusão no grupo e, em seguida, 
+a condição de exclusão. Por exemplo, group="*, !ITUB" significa que primeiro é necessário selecionar as posições de todos os símbolos e, em seguida, 
+excluir as que contêm o símbolo "ITUB" no nome.deve-se especificar as condições para inclusão no grupo e, em seguida, a condição de exclusão. 
+Por exemplo, group="*, !ITUB" significa que primeiro é necessário selecionar as posições de todos os símbolos e, em seguida, excluir as que contêm o símbolo "ITUB" no nome.
+'''
 usd_positions=mt5.positions_get(group="group_search")
 if usd_positions==None:
     print(f"Sem positions com o parâmetro group={group_search}, error code={mt5.last_error()}")
@@ -30,7 +39,6 @@ else:
     df=pd.DataFrame(list(usd_positions),columns=usd_positions[0]._asdict().keys())
     df['time'] = pd.to_datetime(df['time'], unit='s')
     df.drop(['time_update', 'time_msc', 'time_update_msc', 'external_id'], axis=1, inplace=True)
-    print(df)
  
 # A pesquisa por bilhete necessita de um número específico de operação, porém o padrão seria
 # mt5.positions_get(ticket=0000000)
